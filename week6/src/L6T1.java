aec9053ec7cce5ac3dae344d4569cf3118026b8e
Tamako231 <a1131162437@163.com>
2024-10-14 10:38:48 +0800

    Add files via upload

---
+++ week6/src/L6T1.java
@@ -0,0 +1,11 @@
+public class L6T1 {
+    public static void main(String[] args) {
+        int sum=0,i=1;
+        while(i<10){
+            sum=sum+1;
+            i++;
+
+        }
+        System.out.println("sum is"+sum);
+    }
+}
